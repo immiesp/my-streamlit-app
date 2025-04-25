258b029f16324e80fa72679c0378f9fb078cc360
immiesp <samachayapra@gmail.com>
2025-04-25 23:55:43 +0700

    Add files via upload

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
+import plotly.express as px 
 
 st.title('Uber pickups in NYC')
  
@@ -49,6 +51,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import plotly.express as px 
  
 chart_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
